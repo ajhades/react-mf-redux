a0ab25be169a24607d1d31398cc23a900d8f1617
Alvaro Borrero <ajhades@hotmail.com>
2024-11-12 14:51:51 -0500

    config y adicion del nuevo micro front

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
+import { lazy, Suspense } from "react";
 import TodoApp from "./components/TodoApp/TodoApp";
 import { Provider } from "react-redux";
 import store from "./redux/store";
+const RemoteApp = lazy(() => import("app_remote/RemoteApp"));
 
 function App() {
   return (
@@ -8,6 +10,9 @@ function App() {
       <div className="container">
         <h1>Lista de quehaceres</h1>
         <TodoApp />
+        <Suspense fallback={<div>Loading Remote App...</div>}>
+          <RemoteApp />
+        </Suspense>
       </div>
     </Provider>
   );
